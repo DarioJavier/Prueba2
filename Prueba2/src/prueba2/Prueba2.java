713821915851a1f3ece29856de5c975f40110ed7
DarioJavier <ramirezdario93@gmail.com>
2014-11-06 17:07:18 +0100

    cambio dos mensajes

--- Prueba2/src/prueba2/Prueba2.java
+++ Prueba2/src/prueba2/Prueba2.java
@@ -17,6 +17,7 @@ public class Prueba2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("fdfdfdfljfogjfogj");
+         System.out.println("hola mundo");
     }
     
 }
